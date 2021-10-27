6986fbf2629c4ab93464b039272de6c465a49a0d
buidennis <dennistbui@gmail.com>
2021-10-26 22:17:55 -0700

    Added model, Room, to the API application

--- api/models.py
+++ api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import string
+import random
 
-# Create your models here.
+
+def generate_unique_code():
+    '''
+    Input: None
+    Output: code, string of size defined by length that is unique
+            in the set of Room models
+    '''
+    length = 10
+    while True:
+        # Creates a code with length k using only upper case ascii letters
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+
+        # Calculates the number of Room models that contain the generated code
+        if Room.objects.filter(code=code).count == 0:
+            break
+
+        return code
+
+
+class Room(models.Model):
+    code = models.CharField(max_length=8, default="", unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
